f92293860df1cda729b210ba2693c78b7f9e8a53
deniloss <danil.sitnik.work@gmail.com>
2022-05-27 11:33:39 +0300

    feat: Добавлены роуты

---
+++ routes/movies.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const { celebrate, Joi} = require('celebrate');
+const validator = require('validator');
+const { getMovies, postMovie, deleteMovie } = require('../controllers/movies');
+
+router.get('/movies', getMovies);
+router.post(
+  '/movies',
+  celebrate({
+    body: Joi.object().keys({
+      country: Joi.string().required(),
+      director: Joi.string().required(),
+      duration: Joi.number().required(),
+      year: Joi.number().required(),
+      description: Joi.string().required(),
+      image: Joi.string()
+        .required()
+        .custom((value, helpers) => {
+          if (validator.isURL(value)) {
+            return value;
+          }
+          return helpers.message('Изображение должно быть указано ссылкой');
+        }),
+      trailer: Joi.string()
+        .required()
+        .custom((value, helpers) => {
+          if (validator.isURL(value)) {
+            return value;
+          }
+          return helpers.message('Трейлер должен быть передан ссылкой');
+        }),
+      thumbnail: Joi.string().required().custom((value, helpers) => {
+        if (validator.isURL(value)) {
+          return value;
+        }
+        return helpers.message('Миниатюра должна быть передана ссылкой');
+      }),
+      movieId: Joi.number().required(),
+      nameRU: Joi.string().required(),
+      nameEN: Joi.string().required(),
+    }),
+  }),
+  postMovie,
+);
+
+router.delete(
+  '/movies/:movieId',
+  celebrate({
+    params: Joi.object().keys({
+      movieId: Joi.string().length(24).hex(),
+    }),
+  }),
+  deleteMovie,
+);
+
+module.exports = router;
