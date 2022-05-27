6a9785f4250e52ec9cc88bbd2017288919343f26
deniloss <danil.sitnik.work@gmail.com>
2022-05-27 10:55:32 +0300

    feat: Добавлен контроллер movie

---
+++ controllers/movies.js
@@ -0,0 +1,65 @@
+const BadRequestError = require('../errors/bad-request-err');
+const NotFoundError = require('../errors/not-found-err');
+const { Movie } = require('../models/movie');
+
+exports.getMovies = (req, res, next) => {
+  const owner = req.user._id;
+  Movie.find({ owner })
+    .then((movies) => res.status(200).send(movies))
+    .catch(next);
+};
+
+exports.deleteMovie = (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .then((movie) => {
+      if (!movie || movie.owner.toString() !== req.user._id) {
+        throw new NotFoundError('Вы не можете удалять чужие фильмы');
+      }
+      movie
+        .remove()
+        .then(() => {
+          res.send({ message: 'Фильм удалён' });
+        })
+        .catch(next);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        throw new BadRequestError('Данные введены не корректно');
+      }
+      throw err;
+    })
+    .catch(next);
+};
+
+exports.postMovie = (req, res, next) => {
+    const {
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailer,
+      thumbnail,
+      movieId,
+      nameRU,
+      nameEN,
+    } = req.body;
+    const owner = req.user._id;
+    Movie.create({
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailer,
+      thumbnail,
+      owner,
+      movieId,
+      nameRU,
+      nameEN,
+    })
+      .then((movie) => res.send({ data: movie }))
+      .catch(next);
+  };
